a28ac9826f6067ad3dbeb88b866260d689bc0344
Modou Lamin Jaiteh <167339136+modouljaiteh24@users.noreply.github.com>
2025-05-21 16:56:31 +0000

    working on the product images functionality

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+const mainImage = document.getElementById("main-image");
+const imageProduct1 = document.getElementById("image-product-1");
+const imageProduct2 = document.getElementById("image-product-2");
+const imageProduct3 = document.getElementById("image-product-3");
+const imageProduct4 = document.getElementById("image-product-4");
